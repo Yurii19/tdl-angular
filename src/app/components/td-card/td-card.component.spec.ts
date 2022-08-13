2c477112991d784413aea51818fc5bf2851c71dd
Yurii19 <yurikolechkin@gmail.com>
2022-08-13 13:03:43 +0300

    feature: card component created

---
+++ src/app/components/td-card/td-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TdCardComponent } from './td-card.component';
+
+describe('TdCardComponent', () => {
+  let component: TdCardComponent;
+  let fixture: ComponentFixture<TdCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TdCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TdCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/td-card/td-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-td-card',
+  templateUrl: './td-card.component.html',
+  styleUrls: ['./td-card.component.scss']
+})
+export class TdCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
