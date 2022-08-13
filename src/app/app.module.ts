b31d3a046f900ba955a728b9b1505e04aff56640
Yurii19 <yurikolechkin@gmail.com>
2022-08-13 13:42:45 +0300

    feature: added layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TdCardComponent } from './components/td-card/td-card.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TdCardComponent
+    TdCardComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatCardModule
+  ],
+  exports: [
+    TdCardComponent,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
