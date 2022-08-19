98c37dc67f607cb1533b79ee366d610b3e728a80
Yurii19 <yurikolechkin@gmail.com>
2022-08-19 15:55:31 +0300

    feature: done tasks now is placed after to do tasks

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -3,7 +3,7 @@ import { Subject } from 'rxjs';
 import { IAppState, ITask } from 'src/app/Interfaces';
 import { selectTasks } from 'src/app/components/task.selector';
 import { Store } from '@ngrx/store';
-import { takeUntil } from 'rxjs/operators';
+import { map, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-layout',
@@ -14,17 +14,18 @@ export class LayoutComponent implements OnInit, OnDestroy {
   tasks!: ITask[];
   destroy$: Subject<any> = new Subject<any>();
   newTaskId!: Number;
-  tasksComposition!: {completed: Number, toDo: Number};
+  tasksComposition!: { completed: Number; toDo: Number };
 
   constructor(private store: Store<IAppState>) {
     this.store
       .select(selectTasks)
       .pipe(takeUntil(this.destroy$))
+      .pipe(map((data) => this.separateByStatus (data)))
       .subscribe((data) => {
         this.tasks = data;
         this.newTaskId = this.getNewTaskId();
-        this.tasksComposition = this.getTasksComposition(data)
-        console.log('new tasks -> ', data)
+        this.tasksComposition = this.getTasksComposition(data);
+        console.log('new tasks -> ', data); //to do: remove the line
       });
   }
 
@@ -37,24 +38,29 @@ export class LayoutComponent implements OnInit, OnDestroy {
     this.destroy$.unsubscribe();
   }
 
-  getTasksComposition(data: ITask []) {
-    const completed : Number = data.filter(el => el.status === 'done').length;
-    console.log('---- ', completed)
-    const toDo : Number = data.length - (+completed);
-    return {completed , toDo}
+  getTasksComposition(data: ITask[]) {
+    const completed: Number = data.filter((el) => el.status === 'done').length;
+    const toDo: Number = data.length - +completed;
+    return { completed, toDo };
   }
 
   getNewTaskId(): Number {
     let newTaskId: number = -1;
-    const arr = this.tasks.map((el) => el.id);
-    for (let i = 0; i < arr.length; i++) {
-      if (!arr.includes(i)) {
+    const arrOFTaskIndexes = this.tasks.map((el) => el.id);
+    for (let i = 0; i < arrOFTaskIndexes.length; i++) {
+      if (!arrOFTaskIndexes.includes(i)) {
         newTaskId = i;
       }
     }
     if (newTaskId < 0) {
-      return arr.length;
+      newTaskId = arrOFTaskIndexes.length;
     }
     return newTaskId;
   }
-}
+
+  separateByStatus(data: ITask[]): ITask[] {
+    const completed = data.filter((el) => el.status === 'done');
+    const toDo = data.filter((el) => el.status === 'to do');
+    return [...toDo, ...completed];
+  }
+} 
